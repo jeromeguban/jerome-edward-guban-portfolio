b9e169e34b8873bccef79975d1437a113fd8ea9c
Jerome Edward <jeromeedward.guban@hmr.ph>
2025-11-25 00:50:52 +0800

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,29 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Jerome Edward Guban - Portfolio',
   description: 'Backend Developer',
+  metadataBase: new URL('https://jerome-edward-guban-portfolio.vercel.app'),
+  openGraph: {
+    title: 'Jerome Edward Guban - Portfolio',
+    description: 'Backend Developer',
+    url: 'https://jerome-edward-guban-portfolio.vercel.app',
+    siteName: 'Jerome Edward Guban - Portfolio',
+    images: [
+      {
+        url: '/images/featured-image.jpg',
+        width: 1200,
+        height: 1600,
+        alt: 'Jerome Edward Guban - Portfolio',
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jerome Edward Guban - Portfolio',
+    description: 'Backend Developer',
+    images: ['/images/featured-image.jpg'],
+  },
 }
 
 export default function RootLayout({
